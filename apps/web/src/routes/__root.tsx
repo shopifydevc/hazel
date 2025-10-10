8bd5cb7e19122d49ce47b5f218fb45fff88b98be
Makisuo <minetoaster12@gmail.com>
2025-10-11 00:13:10 +0200

    update

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router"
+import { VersionCheck } from "~/components/version-check"
 
 export const Route = createRootRouteWithContext<{}>()({
 	component: () => (
 		<>
 			<Outlet />
+			<VersionCheck />
 			{/* <TanStackRouterDevtools position="top-right" /> */}
 		</>
 	),
