49771554b5d8f330053c1cf74a6283adc368060e
Makisuo <minetoaster12@gmail.com>
2025-12-09 00:36:51 +0100

    remove rpc devtools

--- packages/rpc-devtools/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { RequestDetail } from "./RequestDetail"
-export { RequestList } from "./RequestList"
-export { RpcDevtoolsPanel } from "./RpcDevtoolsPanel"
-export { injectKeyframes, styles } from "./styles"
